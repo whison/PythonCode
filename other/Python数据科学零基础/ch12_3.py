1b2a4a49a849bef3c898b59da63df540b5fae4bc
whison <mohuisheng@163.com>
2020-09-27 08:38:53 +0800

    Python数据科学零基础-2

--- other/Python数据科学零基础/ch12_3.py
+++ other/Python数据科学零基础/ch12_3.py
@@ -5,13 +5,18 @@
 @file: ch12_3.py
 &time: 2020/9/26 17:22
 '''
-class Banks():
-    bankname = 'ABC Bank'
+
+
+class Banks ():
+    BankName = 'ABC Bank'
+
     def __init__(self, username, money):
         self.name = username
         self.balance = money
 
     def get_balance(self):
         return self.balance
-whison_bank = Banks('whison', 10000000)
-print(whison_bank.name.title(), "存款余额是", whison_bank.get_balance())
+
+
+AbcBank = Banks('whison', 10000000)
+print(AbcBank.name.title(), "存款余额是", AbcBank.get_balance())
